4cbab1bf143938d9d9dd802585cb84350cef6316
Talha Khalid <hafiz.talhakh@gmail.com>
2024-08-20 18:27:54 +0500

    Todo APIs done without db

--- app.js
+++ app.js
@@ -1,10 +1,43 @@
 const http = require('http');
+const express = require('express');
+const cors = require('cors');
 
-const server = http.createServer((req, res) => {
-  //   console.log(req);
-  console.log('Hello World');
-  process.exit();
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const TODO_LIST = [];
+
+/**
+ * API to handle
+ * the GET request for todo list
+ */
+app.get('/', function (request, response, next) {
+  if (TODO_LIST.length > 0) {
+    response.status(200).json({ list: TODO_LIST });
+  } else {
+    response
+      .status(200)
+      .json({ message: 'Todo list is empty.', list: TODO_LIST });
+  }
 });
+
+/**
+ * API to handle
+ * the POST request for todo list
+ */
+app.post('/', function (request, response, next) {
+  const item = request.body.item;
+  if (item) {
+    TODO_LIST.push(item);
+    response.status(201).json({ message: 'Success' });
+  } else {
+    response.status(400).json({ message: 'Item is required for this action!' });
+  }
+});
+
+const server = http.createServer(app);
 
 // server.listen(8080)
 server.listen(8080, () => {
